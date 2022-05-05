ff2730ae4ef61105a820b413c12c66c4ddef2173
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-05 11:51:51 +0530

    First commit to main repo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeLadderSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake & Ladder Game Simulator");
+            Console.ReadKey();
+        }
+    }
+}
