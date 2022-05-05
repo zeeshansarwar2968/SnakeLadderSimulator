b1df2dcf06f31fbfaa55a4d5b55922ea851b756d
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-05 12:31:26 +0530

    Added logic to simulate a die roll

--- GameSimulator.cs
+++ GameSimulator.cs
@@ -10,11 +10,21 @@ namespace SnakeLadderSimulator
     {
         //Initialising a variable to hold the value of position of player on the board
         int position = 0;
+
+        // Using random method to generate a random number between 1 and 6
+        Random generateNum = new Random();
+
+        
         
         // Function to create the logic of the game
         public void GameLogic()
         {
+            // Using random method to generate a random number between 1 and 6
+            int dieRoll = generateNum.Next(1, 7);
             Console.WriteLine("The Starting Position of the Player is {0}",position);
+            Console.WriteLine("The value of the die roll is {0}", dieRoll);
+
         }
     }
 }
+                                       
